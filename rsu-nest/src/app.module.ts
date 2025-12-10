d4145043feb829dbcb1080e12ba6ad50f043d298
Josu <jeffcarbajalvicente@gmail.com>
2025-12-10 16:00:47 -0500

    prueba git

--- rsu-nest/src/app.module.ts
+++ rsu-nest/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//se debe poner el mudulo princiapl de la app
+//archivo donde esta ubicado mi base de datos
+//app.module.ts
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.Josue2008,
+      database: process.env.Prueba_db,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
